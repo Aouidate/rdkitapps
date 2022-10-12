609de7f16c1a23a1c6aaa4a4530a636a1c3c240b
Adnane Aouidate <adnane@Adnanes-MacBook-Air.local>
2022-10-12 23:59:05 +0200

    modify the landing page

--- chemoweb.py
+++ chemoweb.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('A little blog for different chemoinformatis apps and codes')
+st.markdown("<h1 style='text-align: center; color: grey;'>BA little blog for different chemoinformatis apps and codes</h1>", unsafe_allow_html=True)
 
-st.markdown("""I will be sharing with you some exciting blogs, codes, articles and also help you to discover the chemoinformatics' world: Here is the screenshot of a streamlit web app that I started to develop which will find many apps and code snippets""")
+st.markdown("<h2 style='text-align: center; color: black;'>I will be sharing with you some exciting blogs, codes, articles and also help you to discover the chemoinformatics' world: Here is the screenshot of a streamlit web app that I started to develop which will find many apps and code snippets I an an organic chemist I statred my journey in molecular modelling and CADD from 2014 during my thesis scince 201 I'm working as postdoctoral researcher (chemoinformatician). </h2>", unsafe_allow_html=True)
